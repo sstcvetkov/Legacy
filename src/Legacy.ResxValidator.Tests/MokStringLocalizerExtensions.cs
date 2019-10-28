e7645ead61c3c75b9dcd33172ee2c4a672872b51
Sergei Tcvetkov <sstcvetkov@gmail.com>
2019-10-28 17:21:59 +0400

    Add base version

---
+++ src/Legacy.ResxValidator.Tests/MokStringLocalizerExtensions.cs
@@ -0,0 +1,39 @@
+using Microsoft.Extensions.Localization;
+using Moq;
+
+namespace Legacy.ResxValidator.Tests
+{
+	public static class MokStringLocalizerExtensions
+	{
+		public static Mock<IStringLocalizer> SetUpDisplayName(this Mock<IStringLocalizer> localizer,
+			string memberName, string value)
+		{
+			if (value != null)
+				localizer.SetUpString(memberName, value, 
+					ResxValidator.Keywords.DisplayName.ToString());
+
+			return localizer;
+		}
+
+		public static Mock<IStringLocalizer> SetUpRule(this Mock<IStringLocalizer> localizer,
+			string memberName, object value, ResxValidator.Keywords suffix, string message)
+		{
+			localizer
+				.SetUpString(memberName, value?.ToString(), suffix.ToString())
+				.SetUpString(memberName, message,
+					suffix + ResxValidator.Keywords.Message.ToString());
+
+			return localizer;
+		}
+
+		private static Mock<IStringLocalizer> SetUpString(this Mock<IStringLocalizer> localizer,
+			string memberName, string value, string suffix)
+		{
+			var key = $"{memberName}{suffix}";
+			localizer.SetupGet(p => p[It.Is<string>(x => x == key)])
+				.Returns(new LocalizedString(memberName, value ?? string.Empty, false));
+
+			return localizer;
+		}
+	}
+}
---
+++ src/Legacy.ResxValidator/ResxAttribute.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Localization;
+using System.ComponentModel.DataAnnotations;
+
+namespace Legacy.ResxValidator
+{
+	public sealed class ResxAttribute : ValidationAttribute
+	{
+		private readonly string _baseName;
+		private string _resourceName;
+
+		public ResxAttribute(string sectionName, string resourceName = null)
+		{
+			_baseName = sectionName;
+			_resourceName = resourceName;
+		}
+
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)  
+		{
+			if (_resourceName == null)
+				_resourceName = validationContext.MemberName;
+			
+			var factory = validationContext
+				.GetService(typeof(IStringLocalizerFactory)) as IStringLocalizerFactory;
+			var localizer = factory?.Create(_baseName,
+				System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
+
+			ErrorMessage = ErrorMessageString;
+			var currentValue = value as string;
+			var validator = new ResxValidator(localizer);
+
+			return validator.IsValid(_resourceName, currentValue, out var message)
+				? ValidationResult.Success
+				: new ValidationResult(message);
+		}
+	}
+}
